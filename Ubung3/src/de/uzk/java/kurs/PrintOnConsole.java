e3f2e176b668f3e2803abd874d07731f5eecc915
Simha <yshant@gmail.com>
2023-11-02 17:12:07 +0100

    Sitzung 4

--- Ubung3/src/de/uzk/java/kurs/PrintOnConsole.java
+++ Ubung3/src/de/uzk/java/kurs/PrintOnConsole.java
@@ -5,13 +5,13 @@ public class PrintOnConsole {
 	void drücken () {
 		System.out.println("Code im PrintOnConsole ".toUpperCase());
 	}
+	
+	void drückenMitWunsch (String meinWunsch) {
+		System.out.println(meinWunsch);
+	}
 }
 
 
-
-//void drücken (String meinsWunsch) {
-//	System.out.println("Code im PrintOnConsole ".toUpperCase() + meinsWunsch);
-//}
 //
 //void serienEven (boolean b, int zahl) {
 //	System.out.println("Code im serienEven " + b + ", die Zahl ist : " + zahl);
